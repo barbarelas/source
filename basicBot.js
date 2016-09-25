b53b1a1394b121fca8580e0143c3062788bfe59c
Martinez <iagogcostah@Gmail.com>
2016-09-25 11:29:44 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -49,7 +49,7 @@
       botName: "BotCyber",
       language: "portuguese",
       chatLink: "https://raw.githubusercontent.com/fecclis/source/master/lang/pt-BR.json",
-      scriptLink: "https://rawgit.com/bscBot/source/master/basicBot.js",
+      scriptLink: "https://rawgit.com/fecclis/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
       startupVolume: 0, // 0-100
